9c6cd7179a128b2c46e306721a7c1684faeb6080
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-02-23 16:00:31 +0100

    Update seaweedfs.sh

--- ct/seaweedfs.sh
+++ ct/seaweedfs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-COMMUNITY_SCRIPTS_URL="${COMMUNITY_SCRIPTS_URL:-https://git.community-scripts.org/community-scripts/ProxmoxVE/raw/branch/main}"
-source <(curl -fsSL "$COMMUNITY_SCRIPTS_URL/misc/build.func")
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: MickLesk (CanbiZ)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
